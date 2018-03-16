fb4a950efc367d31bdc7392478a46b9271b4ccc2
ktsubo <ktsubo@cosmomachia.co.jp>
2018-03-16 12:05:14 +0000

    bootstrap.sh 追加

---
+++ vagrant/bootstrap.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/creationix/nvm.git ~/.nvm
+source ~/.nvm/nvm.sh
+nvm install v9.8.0
+
+#sudo echo "-A INPUT -m state --state NEW -m tcp -p tcp --dport 22 -j ACCEPT" >> /etc/sysconfig/iptables
+#/etc/init.d/iptables restart
